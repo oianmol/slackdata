28556d965e3e4cbcb91b27c37d3fb4b025a27a0b
Anmol Verma <anmol.verma4@gmail.com>
2022-09-16 12:04:14 +0530

    fix: JdbcSqliteDriver

--- src/jvmMain/kotlin/dev/baseio/slackdata/DriverFactory.kt
+++ src/jvmMain/kotlin/dev/baseio/slackdata/DriverFactory.kt
@@ -2,16 +2,19 @@ package dev.baseio.slackdata
 
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
+import java.io.File
 
 actual class DriverFactory {
   actual fun createDriver(schema: SqlDriver.Schema): SqlDriver {
-    return JdbcSqliteDriver(url = "jdbc:sqlite:SlackDB.db").also {
+    val databasePath = File(System.getProperty("java.io.tmpdir"), "ComposeTodoDatabase.db")
+    return JdbcSqliteDriver(url = "jdbc:sqlite:${databasePath.absolutePath}").also {
       schema.create(it)
     }
   }
 
   actual suspend fun createDriverBlocking(schema: SqlDriver.Schema): SqlDriver {
-    return JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY).also {
+    val databasePath = File(System.getProperty("java.io.tmpdir"), "ComposeTodoDatabase.db")
+    return JdbcSqliteDriver(url = "jdbc:sqlite:${databasePath.absolutePath}").also {
       schema.create(it)
     }
   }
