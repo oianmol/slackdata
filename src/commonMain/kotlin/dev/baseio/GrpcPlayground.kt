8de084f3c5bff744b589ea0893aa92f5d4fe360e
Anmol Verma <anmol.verma4@gmail.com>
2022-09-27 14:01:51 +0530

    add: proto updates for slack

--- src/commonMain/kotlin/dev/baseio/GrpcPlayground.kt
+++ src/commonMain/kotlin/dev/baseio/GrpcPlayground.kt
@@ -1,47 +1,17 @@
 package dev.baseio
 
 
-import dev.baseio.slackdata.protos.kmHelloRequest
 import dev.baseio.slackdata.protos.KMHelloServiceStub
-import dev.baseio.slackdata.protos.KMResponse
+import dev.baseio.slackdata.protos.KMSKWorkspaces
+import dev.baseio.slackdata.protos.kmEmpty
 import io.github.timortel.kotlin_multiplatform_grpc_lib.KMChannel
-import io.github.timortel.kotlin_multiplatform_grpc_lib.util.TimeUnit
 
-suspend fun performGreeting(message: String, port: Int): String {
+suspend fun getWorkspaces(address: String = "localhost", port: Int): KMSKWorkspaces {
   val channel = KMChannel.Builder
-    .forAddress("localhost", port)
+    .forAddress(address, port)
     .usePlaintext()
     .build()
 
   val stub = KMHelloServiceStub(channel)
-
-  val request = kmHelloRequest {
-    greeting = message
-  }
-
-  val response: KMResponse = stub
-    .withDeadlineAfter(10, TimeUnit.SECONDS)
-    .sayHello(request)
-
-  return response.response
-}
-
-suspend fun performMultipleGreetings(message: String, port: Int) {
-  val channel = KMChannel.Builder
-    .forAddress("localhost", port)
-    .usePlaintext()
-    .build()
-
-  val stub = KMHelloServiceStub(channel)
-
-  val request = kmHelloRequest {
-    greeting = message
-  }
-
-  stub
-    .withDeadlineAfter(10, TimeUnit.SECONDS)
-    .sayHelloMultipleTimes(request)
-    .collect {
-      println("Received: ${it.response}")
-    }
+  return stub.getWorkspaces(kmEmpty { })
 }
