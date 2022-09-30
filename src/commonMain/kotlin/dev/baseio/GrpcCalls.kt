ee4c9659741d3f5ccf9b716bde35bd81ae501638
Anmol Verma <anmol.verma4@gmail.com>
2022-09-30 23:13:19 +0530

    add: rpc calls

---
+++ src/commonMain/kotlin/dev/baseio/GrpcCalls.kt
@@ -0,0 +1,35 @@
+package dev.baseio
+
+
+import dev.baseio.slackdata.protos.*
+import io.github.timortel.kotlin_multiplatform_grpc_lib.KMChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.runBlocking
+
+const val address = "localhost"
+const val port = 17600
+
+fun channel() = KMChannel.Builder
+  .forAddress(address, port)
+  .usePlaintext()
+  .build()
+
+fun getWorkspaces(): Flow<KMSKWorkspaces> {
+  val channel = channel()
+  val stub = KMWorkspaceServiceStub(channel)
+  return stub.getWorkspaces(kmEmpty { })
+}
+
+fun getChannels(workspaceIdentifier: String): Flow<KMSKChannels> {
+  val channel = channel()
+  val stub = KMChannelsServiceStub(channel)
+  return stub.getChannels(kmSKChannelRequest {
+    workspaceId = workspaceIdentifier
+  })
+}
+
+fun main() {
+  runBlocking {
+    getWorkspaces()
+  }
+}
