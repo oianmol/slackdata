4129ef8aee43137f3d3dd711ea1b68a116eaea01
Anmol Verma <anmol.verma4@gmail.com>
2022-09-30 20:28:07 +0530

    add: MessagingService

--- SlackServer/src/main/kotlin/Main.kt
+++ SlackServer/src/main/kotlin/Main.kt
@@ -1,17 +1,21 @@
 import dev.baseio.slackserver.data.Database
 import dev.baseio.slackserver.data.impl.ChannelsDataSourceImpl
+import dev.baseio.slackserver.data.impl.MessagesDataSourceImpl
 import dev.baseio.slackserver.data.impl.WorkspaceDataSourceImpl
 import dev.baseio.slackserver.services.ChannelService
+import dev.baseio.slackserver.services.MessagingService
 import dev.baseio.slackserver.services.WorkspaceService
 import io.grpc.ServerBuilder
 
 fun main() {
   val workspaceDataSource = WorkspaceDataSourceImpl(Database.slackDB)
   val channelsDataSource = ChannelsDataSourceImpl(Database.slackDB)
+  val messagesDataSource = MessagesDataSourceImpl(Database.slackDB)
 
   ServerBuilder.forPort(17600)
     .addService(WorkspaceService(workspaceDataSource = workspaceDataSource))
     .addService(ChannelService(channelsDataSource = channelsDataSource))
+    .addService(MessagingService(messagesDataSource = messagesDataSource))
     .build()
     .start()
     .awaitTermination()
---
+++ SlackServer/src/main/kotlin/dev/baseio/slackserver/data/impl/MessagesDataSourceImpl.kt
@@ -0,0 +1,6 @@
+package dev.baseio.slackserver.data.impl
+
+import dev.baseio.SlackCloneDB
+import dev.baseio.slackserver.data.MessagesDataSource
+
+class MessagesDataSourceImpl(slackDB: SlackCloneDB) : MessagesDataSource
